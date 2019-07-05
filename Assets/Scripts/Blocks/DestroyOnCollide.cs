0343adc9b51756e3f7c15ad0bba859c934e6eff1
Claudio Domene <claudio79jr@gmail.com>
2019-07-05 13:40:21 -0300

    tilemap e camera

--- Assets/Scripts/Blocks/DestroyOnCollide.cs
+++ Assets/Scripts/Blocks/DestroyOnCollide.cs
@@ -9,8 +9,8 @@ public class DestroyOnCollide : MonoBehaviour
 
     private void DestroyEffect(GameObject mario)
     {
-        if(mario.name == "Mario" || mario.name == "Mario (Clone)" ||
-            mario.name == "FireMario" || mario.name == "FireMario (Clone)")
+        if(mario.name == "Mario" || mario.name == "Mario(Clone)" ||
+            mario.name == "FireMario" || mario.name == "FireMario(Clone)")
             Destroy( gameObject );
     }
 }
---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private GameObject player;
+
+    void Update()
+    {
+        if (player)
+            transform.position = new Vector3(player.transform.position.x,
+                transform.position.y, transform.position.z);
+
+        else
+            player = GameObject.FindWithTag( "Player" );
+    }
+}
