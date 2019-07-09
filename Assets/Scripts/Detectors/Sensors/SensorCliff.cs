f6320f6a001846227069fcbf5a4dafb58b372ad7
Claudio Domene <claudio79jr@gmail.com>
2019-07-08 23:18:55 -0300

    Add - map 1-1

---
+++ Assets/Scripts/Detectors/Sensors/SensorCliff.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorCliff : Sensor
+{
+    [SerializeField]
+    private int count;
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        count--;
+
+        if(count == 0)
+            Trigger( other.gameObject );
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        count++;
+    }
+}
