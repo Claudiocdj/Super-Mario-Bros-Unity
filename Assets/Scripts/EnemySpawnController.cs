7f7d2cf475c47165dd383f7a952e97d6ffe271d6
Claudio Domene <claudio79jr@gmail.com>
2019-07-14 17:03:03 -0300

    Add enemy spawn controller

---
+++ Assets/Scripts/EnemySpawnController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class EnemySpawnController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Enemy")
+        {
+            if(other.GetComponent<AiInputs>())
+                other.transform.GetComponent<AiInputs>().enabled = true;
+            else
+                other.transform.parent.GetComponent<AiInputs>().enabled = true;
+        }
+    }
+}
